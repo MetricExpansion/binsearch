8c46b4dd5dbab24f59083eca23be169926a1b3ef
Avinash Vakil <m@avinashvakil.com>
2021-08-08 00:05:22 -0700

    Format files

--- src/main.rs
+++ src/main.rs
@@ -11,18 +11,30 @@ use structopt::StructOpt;
 static A: AllocCounterSystem = AllocCounterSystem;
 
 #[derive(Debug, StructOpt)]
-#[structopt(name = "pcheck", about = "A tool to find single-precision floating point numbers inside of binary data.")]
+#[structopt(
+    name = "pcheck",
+    about = "A tool to find single-precision floating point numbers inside of binary data."
+)]
 struct Opt {
     #[structopt(parse(from_os_str), help = "The file to search.")]
     input: Option<PathBuf>,
 
-    #[structopt(long, default_value = "1048576", conflicts_with("input"), help = "If no file provided, the number of floats to generate as sample data.")]
+    #[structopt(
+        long,
+        default_value = "1048576",
+        conflicts_with("input"),
+        help = "If no file provided, the number of floats to generate as sample data."
+    )]
     sample_data_length: usize,
 
     #[structopt(long, help = "Minimum value of floats to search for (inclusive).")]
     min: Option<f32>,
 
-    #[structopt(long, required_unless("min"), help = "Maximum value of floats to search for (inclusive).")]
+    #[structopt(
+        long,
+        required_unless("min"),
+        help = "Maximum value of floats to search for (inclusive)."
+    )]
     max: Option<f32>,
 
     #[structopt(long, default_value = "0", help = "Minimum length of run to print.")]
@@ -31,7 +43,7 @@ struct Opt {
 
 fn main() {
     let opt = Opt::from_args();
-    
+
     let bytes = if let Some(path) = &opt.input {
         println!("Loading {}", path.to_str().unwrap_or_else(|| "file."));
         let mut file = BufReader::new(File::open(path).expect("Could not find input file."));
@@ -41,9 +53,14 @@ fn main() {
         data
     } else {
         // Set up example data.
-        println!("No input provided. Generating {} bytes of sample data.", 4*opt.sample_data_length);
+        println!(
+            "No input provided. Generating {} bytes of sample data.",
+            4 * opt.sample_data_length
+        );
         let mut rng = rand::thread_rng();
-        let input: Vec<f32> = (0..opt.sample_data_length).map(|_| rng.gen_range(-100.0..1.0)).collect();
+        let input: Vec<f32> = (0..opt.sample_data_length)
+            .map(|_| rng.gen_range(-100.0..1.0))
+            .collect();
         // let input: Vec<f32> = vec![2.0, -32.4, -32.4, 0.5, 0.4, -3.0, 4.2, 4.4];
         // let input: Vec<f32> = vec![-20.0];
         let bytes: Vec<_> = input
